900384e7b0ab7a6c809f29c412f9bc3d8c4f8bd6
SeasunYeo <seasunyeo@gmail.com>
2018-07-13 15:58:57 +0800

    merge code in self_compression.sh

--- self_compression.sh
+++ self_compression.sh
@@ -18,7 +18,7 @@ if [ -z $filename ]; then
 	Usage;
 elif [ $filename = '--list' ];	then
 	List;
-elif [[ -n $filename && ! -n $path ]]; then
+elif [[ -n $filename ]]; then
 	while  [[ ! "$nextname" == "$filename" ]]
 	do
 		case $extention in 
@@ -56,45 +56,48 @@ elif [[ -n $filename && ! -n $path ]]; then
 		extention="${filename##*.}"
 		let "cycle++"
 	done
+	if [[ -n $path ]]; 	then
+		eval "mv $filename $path"
+	fi
 	echo "right"	
-elif [[ -n $filename && -n $path ]];	then
-	while  [[ ! "$nextname" == "$filename" ]]
-	do
-		case $extention in 
-			'tar')
-				echo "解压tar文件"
-				eval "tar xvf $filename -C $defaultpath";;
-			'gz')
-				echo "解压gz文件"
-				eval "gzip -d $filename";;
-			'bz2')
-				echo "解压bz2文件"
-				eval "bzip2 -d $filename";;
-			'zip')
-				echo "解压zip文件"
-				eval "unzip $filename -d $defaultpath";;
-			'rar')
-				echo "解压rar文件"
-				eval "rar x $filename" $defaultpath;;
-			'bz')
-				echo "解压bz文件"
-				eval "bzip2 -d $filename";;
-			'z')
-				echo "解压z文件"
-				eval "uncompress $filename";;
-			*)
-				if [ "$cycle" -eq 0 ]; then 
-					echo "error(101) This type is not supported(tar|gz|gz2|zip|z|rar|bz|bz2)";
-				else
-					echo "Already done"
-					eval "mv $filename $path"
-					exit;
-				fi
-		esac
-		filename=$nextname
-		nextname="${filename%.*}"
-		extention="${filename##*.}"
-		let "cycle++"
-	done
-	eval "mv $filename $path"
+#elif [[ -n $filename && -n $path ]];	then
+#	while  [[ ! "$nextname" == "$filename" ]]
+#	do
+#		case $extention in 
+#			'tar')
+#				echo "解压tar文件"
+#				eval "tar xvf $filename -C $defaultpath";;
+#			'gz')
+#				echo "解压gz文件"
+#				eval "gzip -d $filename";;
+#			'bz2')
+#				echo "解压bz2文件"
+#				eval "bzip2 -d $filename";;
+#			'zip')
+#				echo "解压zip文件"
+#				eval "unzip $filename -d $defaultpath";;
+#			'rar')
+#				echo "解压rar文件"
+#				eval "rar x $filename" $defaultpath;;
+#			'bz')
+#				echo "解压bz文件"
+#				eval "bzip2 -d $filename";;
+#			'z')
+#				echo "解压z文件"
+#				eval "uncompress $filename";;
+#			*)
+#				if [ "$cycle" -eq 0 ]; then 
+#					echo "error(101) This type is not supported(tar|gz|gz2|zip|z|rar|bz|bz2)";
+#				else
+#					echo "Already done"
+#					eval "mv $filename $path"
+#					exit;
+#				fi
+#		esac
+#		filename=$nextname
+#		nextname="${filename%.*}"
+#		extention="${filename##*.}"
+#		let "cycle++"
+#	done
+#	eval "mv $filename $path"
 fi 
